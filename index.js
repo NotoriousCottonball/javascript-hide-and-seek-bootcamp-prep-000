e0171db46127b9da8105b70e6509cd9de7c9d89e
George  Pianka <gpianka1@gmail.com>
2018-03-09 03:45:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target');
+}
+function increaseRankBy(n) {  
+  let inc=document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i=0; i<inc.length; i++) {
+    inc[i].innerHTML=(parseInt(inc[i].innerHTML)+n).toString();
+  }
+}
+function deepestChild() {
+  var deep=document.getElementById("grand-node").querySelectorAll("div");
+  return deep [deep.length-1];
+}
