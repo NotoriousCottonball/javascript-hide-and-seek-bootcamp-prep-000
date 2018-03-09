df6d4784cd52a93e6eb6f56ac59e46b24f8cc393
George  Pianka <gpianka1@gmail.com>
2018-03-09 03:32:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function nestedTarget() {
   return document.getElementById('nested').querySelector('.target');
 }
 function increaseRankBy(n) {  
-  let inc=document.getElementById('app').querySelectorAll('ul.ranked-list')
+  let inc=document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i=0, i<inc.length; i++) {
+    inc[i].innerHtml=
+  }
   
 }
